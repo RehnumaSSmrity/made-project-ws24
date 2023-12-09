0c6b27ac49b526e459146165c32293638083b3c2
Rehnuma Sharmin <52393403+RehnumaSSmrity@users.noreply.github.com>
2023-12-09 22:43:40 +0100

    Update of ex3

--- exercises/exercise3.py
+++ exercises/exercise3.py
@@ -1,35 +1,38 @@
-# Importing the pandas library
 import pandas as pd
 
-# Define columns to be kept from the dataset
-columns_selected = [0, 1, 2, 12, 22, 32, 42, 52, 62, 72]
+# Define the columns to keep from the dataset
+columns_to_keep = [0, 1, 2, 12, 22, 32, 42, 52, 62, 72]
 
-# Data source URL
+# URL of the data source
 data_url = 'https://www-genesis.destatis.de/genesis/downloads/00/tables/46251-0021_00.csv'
 
-# Load the dataset, specifying necessary parameters
-# Skip the first 6 rows, use specific columns, and set encoding
-vehicle_data = pd.read_csv(data_url, sep=';', encoding='latin-1', dtype={1: str},
-                           skiprows=6, usecols=columns_selected)
+# Load data from the CSV file
+# Skip the first 6 rows, use specific columns, and set proper encoding
+data_frame = pd.read_csv(data_url, sep=';', encoding='latin-1', dtype={1: str},
+                         skiprows=6, usecols=columns_to_keep)
 
-# Drop the last 4 rows from the dataframe
-vehicle_data = vehicle_data[:-4]
+# Drop the last 4 rows of the dataframe
+data_frame = data_frame[:-4]
 
-# Renaming the dataframe columns for clarity
-column_labels = ['date', 'CIN', 'name', 'petrol', 'diesel', 'gas',
-                 'electro', 'hybrid', 'plugInHybrid', 'others']
-vehicle_data.columns = column_labels
+# Rename columns for better readability
+column_names = ['date', 'CIN', 'name', 'petrol', 'diesel', 'gas', 'electro',
+                'hybrid', 'plugInHybrid', 'others']
+data_frame.columns = column_names
 
-# Filter out rows with invalid CIN values (length not equal to 5)
-vehicle_data = vehicle_data[vehicle_data['CIN'].str.len() == 5]
+# Filter out rows with invalid CIN values (not equal to 5 characters)
+data_frame = data_frame[data_frame['CIN'].str.len() == 5]
 
-# Remove rows with non-positive values in specific columns
-fuel_types = ['petrol', 'diesel', 'gas', 'electro', 'hybrid', 'plugInHybrid', 'others']
-for fuel in fuel_types:
-    # Convert columns to integers, replacing non-numeric values
-    vehicle_data[fuel] = pd.to_numeric(vehicle_data[fuel], errors='coerce')
-    # Keep rows with positive values only
-    vehicle_data = vehicle_data[vehicle_data[fuel] > 0]
+# Replace non-numeric values with NaN and then drop these rows
+positive_columns = ['petrol', 'diesel', 'gas', 'electro', 'hybrid', 'plugInHybrid', 'others']
+data_frame[positive_columns] = data_frame[positive_columns].replace('-', pd.NA)
+data_frame.dropna(subset=positive_columns, inplace=True)
 
-# Save the transformed data to a SQLite database
-vehicle_data.to_sql('vehicle_registry', 'sqlite:///cars.sqlite', if_exists='replace', index=False)
+# Convert columns to integers
+data_frame[positive_columns] = data_frame[positive_columns].astype(int)
+
+# Keep only rows with positive values in specified columns
+for column in positive_columns:
+    data_frame = data_frame[data_frame[column] > 0]
+
+# Save the transformed database to an SQLite database
+data_frame.to_sql('cars', 'sqlite:///cars.sqlite', if_exists='replace', index=False)
